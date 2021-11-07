b81112511612da3c9c6a482bc62b75f63dd1bcb2
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2021-11-07 15:45:19 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import AllQuotes from './Pages/AllQuotes';
-import QuoteDetail from './Pages/QuoteDetail';
-import NewQuote from './Pages/NewQuote';
 import Layout from './components/layout/Layout';
-import NotFound from './Pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = lazy(() => import('./Pages/NewQuote'));
+const QuoteDetail = lazy(() => import('./Pages/QuoteDetail'));
+const NotFound = lazy(() => import('./Pages/NotFound'));
+const AllQuotes = lazy(() => import('./Pages/AllQuotes'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route exact path='/quotes'>
-          <AllQuotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className={'centered'}>
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route exact path='/quotes'>
+            <AllQuotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
